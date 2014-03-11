5ade1cad9c7d723dbd5d6014d03d26b825b27286
Stephania Gambaroff Thomas <stgambar@gambaroff.com>
2014-03-10 18:18:27 -0700

    Implement system and year methods.

--- game.rb
+++ game.rb
@@ -3,5 +3,15 @@ class Game
   
   def initialize(name)
     @name = name
+    @year = nil
+    @system = nil
+  end
+  
+  def year(value)
+    @year = value
+  end
+  
+  def system(value)
+    @system = value
   end
 end
--- games_dsl.rb
+++ games_dsl.rb
@@ -2,5 +2,6 @@ LIBRARY = Library.new
 
 def add_game(name)
   game = Game.new(name)
+  game.instance_eval(&block)
   LIBRARY.add_game(game)
 end
